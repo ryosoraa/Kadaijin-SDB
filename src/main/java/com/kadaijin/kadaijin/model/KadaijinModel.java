aaed2889d82f26cc49387ad807bda4802d990dbe
ryosyntaxx <riodwi12174@gmail.com>
2023-10-12 00:16:46 +0700

    Add model description

--- src/main/java/com/kadaijin/kadaijin/model/KadaijinModel.java
+++ src/main/java/com/kadaijin/kadaijin/model/KadaijinModel.java
@@ -1,5 +1,6 @@
 package com.kadaijin.kadaijin.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,9 +17,12 @@ public class KadaijinModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // untuk menghasilkan nilai id
+    @Schema(name = "ID", example = "1", required = true)
     private Integer Id;
 
+    @Schema(name = "User Email", example = "elda@gmail.com", required = true)
     private String username;
 
+    @Schema(name = "Password Email", example = "elda", required = true)
     private String password;
 }
