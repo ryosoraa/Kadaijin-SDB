80b255ff67060f8d9cc7bbf2736121503da5d27b
ryosyntaxx <riodwi12174@gmail.com>
2023-10-12 01:31:06 +0700

    add sql

---
+++ src/main/java/com/kadaijin/kadaijin/database/foreign.session.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE usagi (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+--     username VARCHAR (50) NOT NULL,
+--     login_result VARCHAR (50) NOT NULL,
+--     timestamp TIMESTAMP
+-- );
+
+
+USE training;
+
+DESC usagi;
