d83477bd09ef718a850709204846575e01fd5e88
ryosyntaxx <riodwi12174@gmail.com>
2023-10-16 00:52:08 +0700

    Add API Responses AND Operations

--- src/main/java/com/kadaijin/kadaijin/controller/LogControl.java
+++ src/main/java/com/kadaijin/kadaijin/controller/LogControl.java
@@ -19,6 +19,8 @@ import com.kadaijin.kadaijin.model.log.UserModel;
 import com.kadaijin.kadaijin.repository.KadaijinRepository;
 import com.kadaijin.kadaijin.service.LogService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/log")
 public class LogControl {
@@ -29,22 +31,34 @@ public class LogControl {
     @Autowired
     KadaijinRepository kadaijinRepository;
 
+    @Operation(summary = "Post Data", description = "Post Data Json")
     @PostMapping
     private void postModel(@RequestBody UserModel userModel) {
         this.logService.logInsert(userModel);
         System.out.println(userModel.toString());
     }
 
+    @Operation(summary = "Alternative login", description = "Insert UserName And Password")
+    @PostMapping("/email")
+    private void postEmail(@RequestParam String email) {
+        UserModel userModel = new UserModel();
+        userModel.setUserName(email);
+        this.logService.logInsert(userModel);
+    }
+
+    @Operation(summary = "Restore All Data", description = "restore all saved data")
     @GetMapping("/all")
     private List<UserDTO> getDataLog() {
         return this.logService.getLog();
     }
 
+    @Operation(summary = "Get Data Log", description = "view login log via email name")
     @GetMapping
     private UserDTO getOne(@RequestParam String request) {
         return this.logService.getOneName(request);
     }
 
+    @Operation(summary = "Returns Email And Log", description = "restore email data and login logs with page")
     @GetMapping("/page")
     private List<UserDTO> getPage(
             @RequestParam(defaultValue = "0") Integer page,
