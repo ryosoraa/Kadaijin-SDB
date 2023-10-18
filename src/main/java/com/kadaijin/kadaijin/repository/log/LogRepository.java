9afe826dfac8b02cfe965b56b091182eb637fcb4
ryosyntaxx <riodwi12174@gmail.com>
2023-10-18 19:39:03 +0700

    Prototipe Range done

--- src/main/java/com/kadaijin/kadaijin/repository/log/LogRepository.java
+++ src/main/java/com/kadaijin/kadaijin/repository/log/LogRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.kadaijin.kadaijin.model.log.LogModel;
+import com.kadaijin.kadaijin.model.log.UserModel;
 
 public interface LogRepository extends JpaRepository<LogModel, Integer> {
 
-    @Query("SELECT l FROM LogModel l WHERE l.timestamp BETWEEN :starting AND :end AND l.userId = :id")
-    List<LogModel> findByTimestampBetweenAndUserId(
-            @Param("starting") String starting,
-            @Param("end") String end,
-            @Param("id") Integer id);
+    @Query("SELECT l FROM LogModel l WHERE l.timestamp BETWEEN :startTime AND :endTime AND l.userId IN :userIds")
+    List<LogModel> findLogsBetweenTimestampsForUsers(@Param("startTime") Timestamp startTime,
+            @Param("endTime") Timestamp endTime,
+            @Param("userIds") UserModel userModel);
 
 }
--- src/main/java/com/kadaijin/kadaijin/service/RangeService.java
+++ src/main/java/com/kadaijin/kadaijin/service/RangeService.java
@@ -1,7 +1,9 @@
 package com.kadaijin.kadaijin.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
+import org.hibernate.mapping.Value;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +12,11 @@ import com.kadaijin.kadaijin.DTO.fiture.ConvertUserDTO;
 import com.kadaijin.kadaijin.DTO.log.LogDTO;
 import com.kadaijin.kadaijin.DTO.log.RangeDTO;
 import com.kadaijin.kadaijin.model.log.LogModel;
+import com.kadaijin.kadaijin.model.log.UserModel;
 import com.kadaijin.kadaijin.repository.log.LogRepository;
 import com.kadaijin.kadaijin.repository.log.UserRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class RangeService {
@@ -25,14 +30,21 @@ public class RangeService {
     @Autowired
     UserRepository userRepository;
 
+    @Transactional
     public RangeDTO restoreAll(String email) {
+
         Integer id = userRepository.findIdByUsername(email);
+        Timestamp starTimestamp = Timestamp.valueOf("2023-10-18 10:44:01");
+        Timestamp endTimestamp = Timestamp.valueOf("2023-10-18 14:34:35");
+        UserModel userModel = new UserModel(id);
         RangeDTO rangeDTO = new RangeDTO();
+        System.out.println("jalan 1");
         List<LogModel> logModel = logRepository
-                .findByTimestampBetweenAndUserId(
-                        "2023-10-18 10:44:01",
-                        "2023-10-18 14:34:35",
-                        id);
+                .findLogsBetweenTimestampsForUsers(
+                        starTimestamp,
+                        endTimestamp,
+                        userModel);
+        System.out.println("jalan 2");
 
         rangeDTO.setUserName(email);
         rangeDTO.setLog(convertUserDTO.listModelToLogDTO(logModel));
